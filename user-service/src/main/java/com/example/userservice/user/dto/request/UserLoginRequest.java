e3676ed3765d59d06e9233d3f8c69b0dd3dc4b56
hyojaeKwon <khj010909@gm.gist.ac.kr>
2024-06-20 16:41:42 +0900

    feat:유저 서비스 test 구현 완료

--- user-service/src/main/java/com/example/userservice/user/dto/request/UserLoginRequest.java
+++ user-service/src/main/java/com/example/userservice/user/dto/request/UserLoginRequest.java
@@ -1,10 +1,12 @@
 package com.example.userservice.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class UserLoginRequest {
 
     private String email;
